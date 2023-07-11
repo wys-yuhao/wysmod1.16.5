d60a834a3d0b29e47a526806fa23c20a25e8d317
wys_yuhao <2354934669@qq.com>
2023-07-11 21:02:27 +0800

    add: admin item - join team

---
+++ src/main/java/cn/wyslmzc/yuhao/wysmod/items/adminItems/JoinRedTeam.java
@@ -0,0 +1,47 @@
+package cn.wyslmzc.yuhao.wysmod.items.adminItems;
+
+import cn.wyslmzc.yuhao.wysmod.utils.PropUtils;
+import cn.wyslmzc.yuhao.wysmod.utils.Tooltip;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class JoinRedTeam extends Item {
+
+    public JoinRedTeam(Properties p_i48487_1_) {
+        super(p_i48487_1_);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack p_77624_1_, @Nullable World p_77624_2_, List<ITextComponent> tooltip, ITooltipFlag p_77624_4_) {
+        Tooltip tip = new Tooltip(tooltip);
+
+        tip.addTip("§a加入§c红队");
+        tip.addInfo("§e看我干蛤qwq");
+
+        tip.show();
+
+        super.appendHoverText(p_77624_1_, p_77624_2_, tip.getTooltip(), p_77624_4_);
+    }
+
+    @Override
+    public ActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
+        ItemStack itemstack = player.getItemInHand(hand);
+        World playerWorld = player.level;
+
+        player.setItemInHand(hand, ItemStack.EMPTY);
+
+        PropUtils.runCommand(playerWorld, "team join red " + player.getName().getContents());
+        PropUtils.tellRaw(playerWorld, "玩家 §a" + player.getName().getContents() + "§r 已加入§c红队§r!");
+
+        return ActionResult.pass(itemstack);
+    }
+}
